d816d75fb3bb4438cb94afb471eb5c9d650fff10
keira172 <dinhhop172@gmail.com>
2020-06-06 13:12:07 +0700

    first commit

---
+++ view/footer.php
@@ -0,0 +1,27 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-4 wow zoomIn">
+				<img src="images/logo.png">
+				<p>Chất lượng - Đảm bảo - Phục vụ</p>
+				<p>Giới thiệu về ViTech</p>
+				<p>Chính sách đổi trả</p>
+				<p>Giao hàng và thanh toán</p>
+			</div>
+			<div class="col-xs-12 col-sm-4 wow zoomIn mxh">
+				<h3>Follow</h3>
+				<i class="fa fa-facebook"></i><span>Facebook.com/vitech</span><br>
+				<i class="fa fa-instagram"></i><span>Instagram.com/vitech</span><br>
+				<i class="fa fa-twitter"></i><span>Twitter.com/vitech</span><br>
+			</div>
+			<div class="col-xs-12 col-sm-4 wow zoomIn mxh">
+				<h3>Liên hệ</h3>
+				<i class="fa fa-map-marker"></i><span>123 Quang Trung, Gò Vấp, Hồ Chí Minh</span><br>
+				<i class="fa fa-map-marker"></i><span>456 Quang Trung, Gò Vấp, Hồ Chí Minh</span><br>
+				<i class="fa fa-phone"></i><span>01655205223</span>
+			</div>
+			<div class="col-xs-12 wow zoomIn"><p>Webssite thiết kế bởi Tô Tấn Vĩ</p></div>
+		</div>
+	</div>
+	<button onclick="topFunction()" id="backtotop" title="Go to top" class="btn"><i class="fa fa-arrow-up"></i></button>
+</footer>
